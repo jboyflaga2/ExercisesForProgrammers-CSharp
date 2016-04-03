1cef052843f568e78afa4c243c806595eba7289b
jboy flaga <jboyflaga2@gmail.com>
2016-04-04 00:46:36 +0800

    exercise 1

---
+++ 01-SayingHello/src/SayingHello/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace SayingHello
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Hello, {0}, nice to meet you!", Console.ReadLine());            
+        }
+    }
+    
+    /*
+    public class Program
+    {        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("Hello, {0}, nice to meet you!", Console.ReadLine());
+            
+            //for Console.Out cf. https://msdn.microsoft.com/en-us/library/system.console.out(v=vs.110).aspx            
+            StreamWriter writer = new StreamWriter(Console.OpenStandardOutput());
+            sw.AutoFlush = true;
+            Console.SetOut(sw);
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());            
+            
+            Greeter greeting = new Greeter(sw, Console.In);
+            greeting.DisplayGreeting(string.Format("Hello, {0}, nice to meet you!", greeting.GetName("What is your name?")));
+        }
+    }
+    
+    public class Greeter
+    {
+        StreamWriter _outputStream;
+        StreamReader _inputStream;
+        
+        public Greeter(StreamWriter outputStream, StreamReader inputStream)
+        {
+            _outputStream = outputStream;
+            _inputStream = inputStream;
+        }
+        
+        public string GetName(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
+        }
+        
+        public void DisplayGreeting(string greeting)
+        {
+            Console.WriteLine(greeting);
+        }
+    }
+    */
+}
---
+++ 01-SayingHello/test/SayingHelloTests/GreeterTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace SayingHelloTest
+{
+    public class GreeterTest
+    {
+        [Fact]
+        public void AskName_ReturnsNameOfUser()
+        {
+            //Greeter greeter = new Greeter(StreamWriter outputStream, StreamReader inputStream);
+            
+            //Xunit is not yet working
+            //d:\Github\ExercisesForProgrammers-CSharp\01-SayingHello\test\SayingHelloTests\project.json(9,18): error NU1002: The dependency xunit.abstractions 2.0.0 does not
+            //support framework DNXCore,Version=v5.0.
+            
+            
+        }
+    }
+}
